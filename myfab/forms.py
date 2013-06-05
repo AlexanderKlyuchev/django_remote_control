027c1e536fdaa89a31adfd4e84c3c894dfa392d8
Alexander <aklyuchev@mail.ru>
2013-06-05 15:14:38 +0400

    removed trash from forms

--- myfab/forms.py
+++ myfab/forms.py
@@ -1,13 +1,7 @@
 from django import forms
-from django.contrib.humanize.templatetags.humanize import apnumber
-from django.template.defaultfilters import pluralize
-
 from django.db import models
-from django.utils.text import capfirst
-from django.core import exceptions
 from myfab.models import Server
 
-#from contentitems.forms import MultiSelectFormField
 
 class Console(forms.Form):
         OPTIONS = (
