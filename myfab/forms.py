edd9ae6c9e4f373e9dc4436a9cf1d25b95469fee
Alexander <aklyuchev@mail.ru>
2013-06-05 15:16:56 +0400

    removed trash from forms

--- myfab/forms.py
+++ myfab/forms.py
@@ -4,10 +4,6 @@ from myfab.models import Server
 
 
 class Console(forms.Form):
-        OPTIONS = (
-            ("a", "A"),
-            ("b", "B"),
-            )
         servers = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple,
                                          queryset = Server.objects.all(),required=True)
         command = forms.CharField()
